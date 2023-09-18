c05873f5a6055f120bc6f4e37032e55d22f59c45
Kevin Thomas <kevinthomas0420@gmail.com>
2023-09-18 21:13:04 +0530

    used axios to fetch movies

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AppBar, Box, Toolbar } from "@mui/material";
 import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
+import { getAllMovies } from "../api-helpers/api-helpers.js";
 
 const top100Films = ["Godfather", "Batman", "Avengers"];
 
 const Header = () => {
   const [value, setValue] = useState(0);
+  const [movies,setMovies] = useState([]);
+
+  useEffect(() => {
+    getAllMovies()
+      .then((data) => setMovies(data.movies))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <AppBar sx={{ bgcolor: "#2b2d42" }}>
-      <Toolbar sx={{ marginLeft: "70px", marginRight: "70px" }}>
+      <Toolbar sx={{ marginLeft: "40px", marginRight: "40px" }}>
         <Box width={"20%"}>
           <VideoLibraryIcon sx={{ fontSize: "35px", cursor: "pointer" }} />
         </Box>
@@ -20,7 +29,7 @@ const Header = () => {
           <Autocomplete
             id="free-solo-demo"
             freeSolo
-            options={top100Films.map((option) => option)}
+            options={movies && movies.map((option) => option.title)}
             renderInput={(params) => (
               <TextField
                 sx={{ input: { color: "white" } }}
