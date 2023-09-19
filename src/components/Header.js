e1c792d1393bc939cd1ba2e4e9e96d32b5fb80fb
Kevin Thomas <kevinthomas0420@gmail.com>
2023-09-19 19:16:24 +0530

    added logout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,15 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import { getAllMovies } from "../api-helpers/api-helpers.js";
 import { Link } from "react-router-dom";
-
+import { useDispatch, useSelector } from "react-redux";
+import { adminActions, userActions } from "../Store/index.js";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const isAdminLoggedIn = useSelector((state) => state.admin.isLoggedIn);
+  const isUserLoggedIn = useSelector((state) => state.user.isLoggedIn);
+
   const [value, setValue] = useState(0);
   const [movies, setMovies] = useState([]);
 
@@ -19,8 +25,15 @@ const Header = () => {
       .catch((err) => console.log(err));
   }, []);
 
+  const logout = (isAdmin) => {
+    dispatch(isAdmin ? adminActions.logout() : userActions.logout());
+  };
+
   return (
-    <AppBar position="sticky"  sx={{ bgcolor: "#2b2d42",height:"90px",justifyContent:"center" }}>
+    <AppBar
+      position="sticky"
+      sx={{ bgcolor: "#2b2d42", height: "90px", justifyContent: "center" }}
+    >
       <Toolbar sx={{ marginLeft: "40px", marginRight: "40px" }}>
         <Box width={"20%"}>
           <VideoLibraryIcon sx={{ fontSize: "35px", cursor: "pointer" }} />
@@ -48,8 +61,37 @@ const Header = () => {
             onChange={(e, val) => setValue(val)}
           >
             <Tab LinkComponent={Link} to="/movies" label="Movies" />
-            <Tab LinkComponent={Link} to="/admin"  label="Admin" />
-            <Tab LinkComponent={Link} to="/auth"  label="Auth" />
+            {!isAdminLoggedIn && !isUserLoggedIn && (
+              <>
+                <Tab LinkComponent={Link} to="/admin" label="Admin" />
+                <Tab LinkComponent={Link} to="/auth" label="Auth" />
+              </>
+            )}
+            {isUserLoggedIn && (
+              <>
+                <Tab LinkComponent={Link} to="/user" label="Profile" />
+                <Tab
+                  onClick={() => logout(false)}
+                  LinkComponent={Link}
+                  to="/"
+                  label="Logout"
+                />
+              </>
+            )}
+            {isAdminLoggedIn && (
+              <>
+                <Tab LinkComponent={Link} to="/add" label="Add Movie" />
+                <Tab LinkComponent={Link} to="/admin" label="Profile" />
+                <Tab
+                  onClick={() => {
+                    logout(true);
+                  }}
+                  LinkComponent={Link}
+                  to="/"
+                  label="Logout"
+                />
+              </>
+            )}
           </Tabs>
         </Box>
       </Toolbar>
